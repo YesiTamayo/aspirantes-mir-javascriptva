bfb683e3a9f177038851ab9417627e621e1fbc2e
YesiTamayo <112041102+YesiTamayo@users.noreply.github.com>
2023-05-12 15:00:06 +0200

    Create conditionals.js

---
+++ js-1/conditionals.js
@@ -0,0 +1,11 @@
+function calcularColor(numero) {
+  if (numero === 1) {
+    return "El color es negro";
+  } else if (numero === 2) {
+    return "El color es blanco";
+  } else if (numero === 3) {
+    return "El color es azul";
+  } else {
+    return "El color es verde";
+  }
+}
